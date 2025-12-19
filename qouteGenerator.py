4410e216b4d7519771f9f583e2eda8f1e0897e39
radhikajadhav00 <radhikajadhav2004@gmail.com>
2025-12-19 10:20:08 +0530

    making an qoute generator program

---
+++ qouteGenerator.py
@@ -0,0 +1,12 @@
+import random
+
+quotes = [
+    "Believe in yourself.",
+    "Small steps every day lead to big success.",
+    "Consistency is more important than speed.",
+    "Don’t stop until you’re proud.",
+    "Your hard work will pay off."
+]
+
+print("🌞 Today's Motivation:")
+print(random.choice(quotes))
