b0c6462f39cff0ffa7170410344daa91884c5a97
radhikajadhav00 <radhikajadhav2004@gmail.com>
2025-12-23 10:17:49 +0530

    Traffic Light

---
+++ TrafficLight.py
@@ -0,0 +1,10 @@
+signal = input("Enter traffic light color: ")
+
+if signal == "red":
+    print("Stop")
+elif signal == "yellow":
+    print("Get Ready")
+elif signal == "green":
+    print("Go")
+else:
+    print("Invalid Signal")
