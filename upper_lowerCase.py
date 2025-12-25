7ab7ba20a06558b4fa978233f96ac5fa623b719f
radhikajadhav00 <radhikajadhav2004@gmail.com>
2025-12-25 10:40:11 +0530

    program for character is uppercase or lowercase

---
+++ upper_lowerCase.py
@@ -0,0 +1,8 @@
+ch = input("Enter a character: ")
+
+if ch >= 'A' and ch <= 'Z':
+    print("The character is Uppercase")
+elif ch >= 'a' and ch <= 'z':
+    print("The character is Lowercase")
+else:
+    print("Not an alphabet character")
